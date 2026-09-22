f00ec23c1ec854d9a84e3ceaf8efb6fd88a2d630
Uener Regly <reglycode@gmail.com>
2026-09-22 10:53:01 -0300

    Main View Controller modified

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,10 @@
 module a379_appJavafxJdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+
 	opens application to javafx.graphics, javafx.fxml;
+
+	opens gui to javafx.fxml;
+
+	exports gui;
 }
